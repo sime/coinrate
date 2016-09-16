6dd0b90946f6fce098a5cd7f4291381f8a4431d3
Simon Males <sime@sime.net.au>
2016-09-16 12:29:41 +0200

    Cache clearer

--- sw.js
+++ sw.js
@@ -47,5 +47,19 @@
     );
   });
 
+  self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
 }());
 
